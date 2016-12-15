5dbb2e937640a213e670d90b882d83ab37d9b27c
Edouard Pelosi <edouard.pelosi@gmail.com>
2016-12-15 15:31:59 +0900

    move out `current_handicap`

--- lib/handicapper/handicap_calculator.rb
+++ lib/handicapper/handicap_calculator.rb
@@ -1,12 +1,16 @@
 module Handicapper
   class HandicapCalculator
+    USGA_OFFICIAL_HDCP_ADJUSTER = 0.96
 
+    # @param [Array] differentials array of float representing the previously calculater hdcp differentials for this player
     def initialize(differentials = [])
       @differentials = differentials
     end
 
-    USGA_OFFICIAL_HDCP_ADJUSTER = 0.96
 
+    # @param [RoundSettings] setting to use for the played round
+    # @param [Array] scores array of ints representing the scores for this round
+    # @return [Float] the handicap after accounting for this new round
     def calculate(setting, scores)
       total_score = if scores.is_a?(Enumerable)
                       setting.adjust_gross_scores(scores)
@@ -16,6 +20,10 @@ module Handicapper
                       return
                     end
       @differentials << settings.handicap_differential(total_score)
+      current_handicap
+    end
+
+    def current_handicap
       d_count = differencials_to_consider
       return unless d_count > 0
       ((@differentials.min(d_count).inject(&:+) / d_count.to_f) * USGA_OFFICIAL_HDCP_ADJUSTER).round(1)
