9b421596ad39ba77d965cf451f05f8d13c20bbd3
Edouard Pelosi <edouard.pelosi@gmail.com>
2016-12-16 10:05:12 +0900

    utility methods on Handicapper itself edit readme

--- lib/handicapper.rb
+++ lib/handicapper.rb
@@ -1,5 +1,14 @@
 require 'handicapper/version'
+require 'handicapper/round_settings'
 
 module Handicapper
+  class << self
+    def round_settings(course_rating, slope_rating, holes_par)
+      RoundSettings.new(course_rating, slope_rating, holes_par)
+    end
 
+    def calculator(differentials = [])
+      HandicapCalculator.new(differentials)
+    end
+  end
 end
