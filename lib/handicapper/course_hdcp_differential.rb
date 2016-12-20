73fea3c9a125c6f0ac9cb451fb0d4a8eed7e532f
Edouard Pelosi <edouard.pelosi@gmail.com>
2016-12-20 18:51:18 +0900

    fix differential calc

--- lib/handicapper/course_hdcp_differential.rb
+++ lib/handicapper/course_hdcp_differential.rb
@@ -8,10 +8,11 @@ module Handicapper
 
     STANDARD_DIFFICULTY_STANDARD = 113.0
 
-    def handicap_differential(total_score)
-      fail ArgumentError, 'Need a valid score for calculating differential' unless total_score.is_a?(Numeric)
-      result = total_score.to_f / course_rating
-      result *= STANDARD_DIFFICULTY_STANDARD / slope_rating
+    def handicap_differential(adjusted_gross_score)
+      fail ArgumentError, 'Need a valid score for calculating differential' unless adjusted_gross_score.is_a?(Numeric)
+      result = adjusted_gross_score.to_f - course_rating.to_f
+      result *= STANDARD_DIFFICULTY_STANDARD
+      result /= slope_rating.to_f
       result.round(1)
     end
   end
--- spec/handicapper/course_hdcp_differential_spec.rb
+++ spec/handicapper/course_hdcp_differential_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 require 'handicapper/course_hdcp_differential'
 describe Handicapper::CourseHdcpDifferential do
-  let(:instance) do
+
+  def make_instance(course_rating, slope)
     Class.new do
       include Handicapper::CourseHdcpDifferential
 
-      def initialize
-        @course_rating= (100.0..155.0).step(0.1).to_a.sample
-        @slope_rating = 72.0 + (-5.0..5.0).step(0.1).to_a.sample
+      def initialize(cr, s)
+        @course_rating= cr.to_f
+        @slope_rating = s.to_f
       end
-    end.new
+    end.new(course_rating, slope)
   end
-  let(:score) { (54..130).to_a.sample }
 
+  let(:instance) { make_instance(72.0 + (-5.0..5.0).step(0.1).to_a.sample, (100.0..155.0).step(0.1).to_a.sample) }
+  let(:score) { (54..130).to_a.sample }
 
   it 'has a course_rating attribute' do
     expect(instance).to respond_to(:course_rating)
@@ -36,5 +38,10 @@ describe Handicapper::CourseHdcpDifferential do
     it 'fails if input is not a number' do
       expect { instance.handicap_differential('Salsa de fuego') }.to raise_exception(ArgumentError)
     end
+
+    it 'should be correct' do
+      expect(make_instance(67.2, 113.0).handicap_differential(83)).to eql(15.8)
+    end
+
   end
 end
