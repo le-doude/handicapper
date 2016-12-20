d173e994c9bd459a3f000d5bff574bb1855f77db
Edouard Pelosi <edouard.pelosi@gmail.com>
2016-12-20 09:24:50 +0900

    sligth refactor of calculator

--- lib/handicapper/handicap_calculator.rb
+++ lib/handicapper/handicap_calculator.rb
@@ -11,6 +11,7 @@ module Handicapper
     # @param [Array] differentials array of float representing the previously calculater hdcp differentials for this player
     def initialize(differentials = [])
       @differentials = differentials
+      @last_handicap_calculated = nil
     end
 
 
@@ -18,24 +19,34 @@ module Handicapper
     # @param [Array] scores array of ints representing the scores for this round
     # @return [Float] the handicap after accounting for this new round
     def calculate(setting, scores)
-      total_score = if scores.is_a?(Enumerable)
-                      setting.adjust_gross_scores(current_handicap, scores)
-                    elsif scores.is_a?(Fixnum)
-                      scores
-                    end
-      return unless total_score
+      total_score = total_score(setting, scores)
       @differentials << setting.handicap_differential(total_score)
-      current_handicap
+      @last_handicap_calculated = calculate_from_differentials
     end
 
     def current_handicap
+      if @last_handicap_calculated.nil? && @differentials.size >= 5
+        @last_handicap_calculated = calculate_from_differentials
+      end
+      @last_handicap_calculated
+    end
+
+    private
+
+    def total_score(setting, scores)
+      if scores.is_a?(Enumerable)
+        setting.adjust_gross_scores(current_handicap, scores)
+      elsif scores.is_a?(Fixnum)
+        scores
+      end
+    end
+
+    def calculate_from_differentials
       d_count = number_to_consider
       return unless d_count > 0
       result = (@differentials.min(d_count).inject(&:+) / d_count.to_f) * USGA_OFFICIAL_HDCP_ADJUSTER
       result.chop
     end
-
-    private
 
     def number_to_consider
       size = @differentials.size
